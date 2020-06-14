ededcefc54bf7e292e670542eaf4d1eee9fed9de
root <admin@example.com>
2020-06-14 08:04:56 +0530

    removed extra code

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,25 @@
 console.log('js file loaded!')
 
-fetch('/wether?address=!').then((response)=>{
-    console.log(response)
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data)
-        }else{
-            console.log(data)
-        }
+const form = document.querySelector('form')
+//const input = document.querySelector("#address").value
+
+
+form.addEventListener("submit", (e)=>{ 
+    e.preventDefault()
+    let address = document.querySelector('input').value
+    document.getElementById('errorMessage').innerHTML = ''
+    document.getElementById('successMessage').innerHTML = ''
+    document.getElementById('successPlace').innerHTML = ''
+    fetch('/wether?address='+address).then((response)=>{
+        //console.log(response)
+        response.json().then((data)=>{
+            if(data.error){
+                document.getElementById('errorMessage').innerHTML = data.error
+            }else{
+                document.getElementById('successMessage').innerHTML = data.message
+                document.getElementById('successPlace').innerHTML = data.place
+            }
+        })
     })
 })
-fetch()
+
